7efe9772387471e321e4e963e0d6dde5be870156
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-04-03 01:08:44 +0300

    added event listener to buy ticket button

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 //fetching data
-fetch("http://localhost:3000/films")
+const url = "http://localhost:3000/films"
+fetch(url)
 .then(res => res.json())
 .then(films =>films.forEach(movie => renderMovies(movie)))
 
+
+
+
 //rendering films
 renderMovies = movie =>{
-    const movieList = document.createElement("ul")
-    movieList.className = "list"
-    movieList.innerHTML = `
-    <li> <img src=${movie.poster}>
+    const movieDetail = document.createElement("p")
+    movieDetail.className = "list"
+    movieDetail.innerHTML = `
     <p>
-    title: ${movie.title} <br>
-    run time: ${movie.runtime} <br>
-    capacity:${movie.capacity}<br>
-    showtime:${movie.showtime} <br>
-    sold tickets: ${movie.tickets_sold} <br>
-    description:${movie.description} <br>
+    <img src=${movie.poster}>
+    <strong> Title:</strong> ${movie.title} <br>
+    <strong>Run time:</strong>  ${movie.runtime} <br>
+    <strong>Capacity:</strong> ${movie.capacity}<br>
+    <strong>Showtime:</strong> ${movie.showtime} <br>
+    <strong>Sold tickets: </strong><span> ${movie.tickets_sold} </span><br>
+    <strong>Description:</strong> ${movie.description} <br>
+    <button id="buy">Buy ticket</button>
+    </p>
     `
-    
-    document.getElementById("listMovies").appendChild(movieList)
+    //creating reduce ticket
+    movieDetail.querySelector("#buy").addEventListener('click',()=>{
+        movie.tickets_sold -- ;
+        const leftTickets = movie.tickets_sold
+
+        if
+
+    })
+   
+    document.getElementById("listMovies").appendChild(movieDetail)
+
+    const movieList = document.createElement("li")   
+    movieList.innerHTML=`
+    ${movie.title}
+    `
+
+    document.getElementById("films").appendChild(movieList)
+
 }
- 
+
+
